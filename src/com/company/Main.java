806b81ad52af9e7a1eb5af7f05a0f4a384f5c29c
mariarodina <q6m4?Qskk#>
2022-04-03 03:37:34 +0300

    дз 7. Строки.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,26 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        task3();
+    }
+
+    public static void task12() {
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника " + fullName);
+
+        String upperFullName = fullName.toUpperCase();
+        System.out.println(upperFullName);
+
+
+    }
+
+    public static void task3() {
+        String fullName = "Иванов Семён Семёнович";
+        String changeFullName = fullName.replace("ё", "е");
+        System.out.println(changeFullName);
     }
 }
